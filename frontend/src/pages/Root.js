8ea403800a2fe89b8a873efaa4fad4ef0da8e598
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2020-05-31 22:41:57 +0200

    chore: added routing

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
-import { Button, CssBaseline } from '@material-ui/core';
+import { CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import { routes } from '../routes';
+import theme from '../theme/theme';
+
+import WelcomePage from '../pages/WelcomePage';
+import ErrorPage from '../pages/ErrorPage';
+import LoginPage from '../pages/LoginPage';
+import RegisterPage from '../pages/RegisterPage';
 
 export default class Root extends Component {
   render() {
     return (
-      <div>
+      <BrowserRouter>
         <CssBaseline />
-        <Button variant='contained' color='primary'>
-          Hello World
-        </Button>
-      </div>
+        <ThemeProvider theme={theme}>
+          <Switch>
+            <Route exact path={routes.home} component={WelcomePage} />
+            <Route exact path={routes.login} component={LoginPage} />
+            <Route exact path={routes.register} component={RegisterPage} />
+            <Route component={ErrorPage} />
+          </Switch>
+        </ThemeProvider>
+      </BrowserRouter>
     );
   }
 }
